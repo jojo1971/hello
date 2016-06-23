713f6f438ea8ae9780051e9ec816a058b6bce9d1
jojo1971 <shoun@free.fr>
2016-06-23 23:43:00 +0200

    newTREST

--- COPCOL/recup.php
+++ COPCOL/recup.php
@@ -2,17 +2,16 @@
 $db = new mysqli('localhost', 'root', '', 'test');
 $text = array();
 
-$req = "SELECT * FROM copcol ";
+$req = "SELECT * FROM copcol ORDER BY ID_COP DESC ";
 $result = mysqli_query($db, $req);
 
 while ($all = mysqli_fetch_assoc($result)) {
     $text[] = $all;
 }
 foreach ($text as $mes) {
-    echo $mes['text'];
-    echo '<br>';
-    echo $mes['nom'];
-    echo '<br>';
+    echo '<h4>' . $mes['nom'] . '</h4>';
+    echo '<p>' . $mes['text'] . '</p>';
+
 }
 
 ?>
